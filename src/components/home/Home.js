96c25127e286fafc1de6f06cb0076bd52ce1f305
Kartik <aggarwalkartik96@gmail.com>
2023-10-05 13:08:20 +0530

    footer css and initial banner code

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,23 +1,31 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { memo, useEffect } from "react";
-// import Header from "./Header";
 // import axios from "axios";
 import "./home.css";
-import Header from "../header/Header";
-import Footer from "../footer/Footer";
-// import { useDispatch, useSelector } from "react-redux";
-// import { getInfo } from "../Actions/Actions";
-
+import demoBtn from '../../assets/svg/demo-video-btn.svg';
+import banner from '../../assets/svg/banner-image.svg'
 
 const Home = () => {
 
   return (
-    <div className="home">
-      {/* <Header /> */}
-      <div className="home-header">
-        Home Body
+    <div className="home-container">
+      <div className="home-banner-container">
+        <div className="banner-content">
+          <div className="banner-title">
+            <span>Future of Learning is</span>
+            <span className="orange-title">UnconstrainED</span>
+          </div>
+          <div className="banner-desc">
+            Partnering with teachers to harness the power of 
+            emerging technology to enable human-centred 
+            approach to learning
+          </div>
+          <img className="demo-btn" src={demoBtn} loading="lazy" alt="demo button" />
+        </div>
+        <div className="banner-image-container">
+          <img className="banner-image" src={banner} loading="lazy" alt="banner" />
+        </div>
       </div>
-      {/* <Footer /> */}
     </div>
   );
 };
