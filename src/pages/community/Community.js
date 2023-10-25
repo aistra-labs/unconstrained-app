7b5a92bdd8ba61766c85912f18f7d3e07d615bce
Kartik Aggarwal <kartik@Kartiks-MacBook-Pro.local>
2023-10-25 18:09:26 +0530

    Community bottom section cards component code

--- src/pages/community/Community.js
+++ src/pages/community/Community.js
@@ -4,8 +4,48 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import "./community.css";
 import { images }from "../../components/images";
+import CommunityCard from "../../components/communityCard/CommunityCard";
 
 const Community = () => {
+
+  const cardData = [
+    {
+      header: "Any Bay-Area Community members interested in a meet-up?",
+      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+      imageUrl: '',
+      username: 'Jessica L Williams',
+      designation: 'Student',
+      userPic: images['user-icon1.svg'],
+      postTime: '22'
+    },
+    {
+      header: "Using Apple Pencil with the Draw App",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      imageUrl: images['community-card-img.png'],
+      username: 'Aiony Haust',
+      designation: 'Student',
+      userPic: images['user-icon2.svg'],
+      postTime: '22'
+    },
+    {
+      header: "Any Bay-Area Community members interested in a meet-up?",
+      description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+      imageUrl: '',
+      username: 'Jessica L Williams',
+      designation: 'Student',
+      userPic: images['user-icon1.svg'],
+      postTime: '22'
+    },
+    {
+      header: "Using Apple Pencil with the Draw App",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      imageUrl: images['community-card-img.png'],
+      username: 'Aiony Haust',
+      designation: 'Student',
+      userPic: images['user-icon2.svg'],
+      postTime: '22'
+    },
+  ];
 
   return (
     <div className="community-container">
@@ -34,6 +74,25 @@ const Community = () => {
             <div className="tab">DISCUSSION</div>
             <div className="tab">REFLECTION</div>
           </div>
+        </div>
+      </div>
+      <div className="community-bottom">
+        <div className="community-bottom-header">Community Questions</div>
+        <div className="community-cards">
+          {cardData.map((card, index) => {
+            return(
+              <CommunityCard 
+                key={index}
+                header={card.header}
+                description={card.description}
+                imageUrl={card.imageUrl}
+                username={card.username}
+                designation={card.designation}
+                userPic={card.userPic}
+                postTime={card.postTime}
+              />
+            )
+          })}
         </div>
       </div>
     </div>
