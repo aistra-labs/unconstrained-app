2544589701a8921fd036d3efcf23e7c9ed8fd048
Kartik Aggarwal <kartik@Kartiks-MacBook-Pro.local>
2023-10-26 18:01:36 +0530

    Fiona requested changes and bug fixes

--- src/components/courseCard/CourseCard.js
+++ src/components/courseCard/CourseCard.js
@@ -10,13 +10,13 @@ const CourseCard = ({ imageUrl, header, description, rating, review }) => {
       </div>
       <div className="card-content">
         <h2 className="card-header">{header}</h2>
-        <div className="rating-review">
+        {/* <div className="rating-review">
             <div className="rating-container">
                 <span className="rating">{rating}</span>
                 <img src={images['rating-star.svg']} alt='rating-star' className="rating-star" />
             </div>
             <span className="review">{review} Reviews</span>
-        </div>
+        </div> */}
         <p className="card-desc">{description}</p>
       </div>
     </div>
