70f788df0096254ef97674dd7f6e3a5dfc76374b
Gauri Shinde <gaurishinde@Gauris-MacBook-Pro.local>
2023-12-21 15:29:32 +0530

    fix retry issue

--- src/utils.jsx
+++ src/utils.jsx
@@ -8,30 +8,18 @@ function wait(delay) {
     return new Promise((resolve) => setTimeout(resolve, delay));
 }
 
-function fetchRetry(requestObj, dispatch, delay = 3000, tries = 3) {
-    if (tries <= 0) {
+export function processResponse(resp, dispatch, requestObj, retry = 3) {
+    if (!resp?.status) throw new Error('Invalid response');
+    if (retry <= 0) {
         logout(dispatch);
         throw new Error('Not Authorized');
     }
-    function onError(err) {
-        const triesLeft = tries - 1;
-        if (triesLeft < 0) {
-            logout(dispatch);
-            throw new Error('Not Authorized');
-        }
-        return wait(delay).then(() => fetchRetry(requestObj, delay, triesLeft));
-    }
-    return fetch(...requestObj).catch(onError);
-}
-
-export function processResponse(resp, dispatch, requestObj, retry = 3) {
-    if (!resp?.status) throw new Error('Invalid response');
     else {
         if (resp.status === 400) throw new Error('Invalid input');
         switch (resp?.status) {
             case 401:
             case 403:
-                return fetchRetry(requestObj, dispatch)
+                return wait(3000).then(()=>processResponse(resp, dispatch, requestObj, retry - 1))
             default:
                 return resp.json()
         }
